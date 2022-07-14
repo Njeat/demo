a0d28c45419653b94145117f31ad7cfe61fc10bb
parktj <ekswn120@gmail.com>
2022-07-14 23:20:40 +0900

    modify ignore file3

--- src/main/java/com/example/demo/config/secret/Secret.java
+++ src/main/java/com/example/demo/config/secret/Secret.java
@@ -4,6 +4,6 @@ package com.example.demo.config.secret;
 // TODO: .gitignore에 추가하는거 앚지 마세요!
 public class Secret {
 //    public static String JWT_SECRET_KEY = "UwKYibQQgkW7g-*k.ap9kje-wxBHb9wdXoBT4vnt4P3sJWt-Nu";
-    public static String JWT_SECRET_KEY = "";
+    public static String JWT_SECRET_KEY = "39a2454100e8d3b2448bea9db1897a99b3066653fbaab92f65fc51de33f05ac76aab188e1ceae749427d36869b5a453e3e48053129e88437faaede217812c7fb";
   
 }
